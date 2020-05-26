6ae0c9040c1282923827b3b83e4fd6b79ac4f67f
root <root@DESKTOP-9PQF3MH.localdomain>
2020-05-26 21:54:29 +0530

    employee daily wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -x
 isPresent=1
+isparttime=1
+isfulltime=0
+empwageperhr=20
+workingdays=20
+absent=0
 randomCheck=$((RANDOM%2))
 if [ $isPresent -eq $randomCheck ]
 then
@@ -7,11 +12,25 @@ then
 else
 	echo "Employee is not Present"
 fi
-if [ $ispresent -eq $randomCheck ]
+
+if [ $randomCheck -eq 0 ]
 then
-	empRatePerHr=20
-	empHrs=8;
-	salary=$(($empHrs*$empRatePerHr))
+	empcheck=2
 else
-	salary=0
+	empcheck=$((RANDOM%2))
 fi
+
+case $empcheck in
+	$isfulltime)
+		emphrs=8;
+		;;
+	$isparttime)
+		emphrs=4
+		;;
+	*)
+		emphrs=0
+		;;
+esac
+salaryperday=$(($emphrs*empwageperhr))
+echo $salaryperday		
+     
